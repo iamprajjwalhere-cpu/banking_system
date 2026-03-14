ec562d7e57e25fffa5b66a6eb8b4f1b42022bf6a
iamprajjwalhere-cpu <iamprajjwalhere@gmail.com>
2026-03-14 22:38:30 +0530

    Update models.py

--- banking/models.py
+++ banking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    acc_no = models.CharField(max_length=20, unique=True)
+    balance = models.FloatField(default=0)
+
+class Transaction(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10)  # Deposit / Withdraw
+    amount = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
